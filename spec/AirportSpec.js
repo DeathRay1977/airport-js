ab70ebd9905933010c72ecf1015ae7a87049c8f6
Norm Fasey <norm@fasey.net>
2015-11-10 22:33:41 +0000

    Refactored a couple of things

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -23,6 +23,7 @@ describe("Airport", function() {
     spyOn(plane, 'land');
     spyOn(plane, 'takeOff');
     spyOn(plane, 'isLanded');
+    spyOn(Math, 'random').andReturn(0.9);
   });
 
   it("Should be a able to land a given plane", function() {
@@ -31,18 +32,16 @@ describe("Airport", function() {
   });
 
   it("A landed plane should be in the hangar", function() {
-    spyOn(Math, 'random').andReturn(0.9);
     airport.land(plane);
     expect(airport.isInAirport(plane)).toEqual(true);
   });
 
   it("Should not allow a plane to land when it is stormy", function() {
-    spyOn(Math, 'random').andReturn(0.0);
+    Math.random.andReturn(0.0);
     expect(function(){airport.land(plane);}).toThrow(Error("Too stormy to land"));
   });
 
   it('Should not allow a plane to land when the airport is full', function() {
-    spyOn(Math, 'random').andReturn(0.9);
     for (var i = 0; i < capacity; i++) {
        airport.land(plane);
      }
@@ -54,24 +53,22 @@ describe("Airport", function() {
   });
 
   it("Should tell us when the weather is stormy", function() {
-    spyOn(Math, 'random').andReturn(0.0);
+    Math.random.andReturn(0.0);
     expect(airport.isStormy()).toBe(true);
   });
 
   it("should't allow a plane to take off when it's stormy", function() {
-    spyOn(Math, 'random').andReturn(0.0);
+    Math.random.andReturn(0.0);
     expect(function(){airport.takeOff(plane);}).toThrow(Error("Can't take off. Stormy"));
   });
 
   it("should allow a plane to take off when it's sunny", function() {
-    spyOn(Math, 'random').andReturn(0.9);
     airport.land(plane);
     airport.takeOff(plane);
     expect(airport.isInAirport(plane)).toBe(false);
   });
 
   it("should not allow a plane to take off if its not in the airport", function () {
-    spyOn(Math, 'random').andReturn(0.9);
     expect(function(){airport.takeOff(plane);}).toThrow(Error('Not in airport'));
   });
 
